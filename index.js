621f8ac243d52719f071fb3e997d0b4ab6edf33b
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-01 21:08:47 -0600

    commit tree 8e385aa41b95bfd21df1f2b867f9477615c751ed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 20) + 1;
+
+let num3 = 104
+let num4 = 100
+const mod = num3 % num4
+
+let max = Math.max (5, 10, 15, 20);
